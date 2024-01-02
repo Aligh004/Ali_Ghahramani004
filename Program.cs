71b0219ea75169716f79d415032cfc132b9a555a
Aligh004 <148377795+Aligh004@users.noreply.github.com>
2024-01-02 23:43:23 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+namespace practice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Plz enter a num");
+                int x = int.Parse(Console.ReadLine());
+                int y = fibo(x);
+                Console.WriteLine(y);
+                Console.ReadKey();
+                   
+                    
+
+            }
+        }
+        static int fibo(int x)
+        {
+            if (x == 1)
+                return 0;
+            else if (x == 2)
+                return 1;
+            else if (x > 2)
+                return fibo(x - 1) + fibo(x - 2);
+            else
+                return -1;
+        }
+        
+
+
+    }
+}
